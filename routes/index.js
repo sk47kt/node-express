bcfe8d87c66700983b5749561b391e2c7bec4cf8
테스트 <sk47kt@naver.com>
2022-12-21 19:32:39 +0900

    ddc

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '정성민페이지' });
+  res.render('index', { title: 'test' });
 });
 
 module.exports = router;
