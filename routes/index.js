447c23a7f31c649d43f44df21dff3f952479ad5a
테스트 <sk47kt@naver.com>
2022-12-21 19:35:12 +0900

    index1

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'test' });
+router.use(express.static('src'));
+
+router.get('/', function(req, res){
+    fs.readFile('./src/index.html', (err, data) => {
+        if(err) throw err;
+
+        res.writeHead(200, {
+            'Content-Type' : 'text/html'
+        })
+        .write(data)
+        .end();
+    });
 });
 
 module.exports = router;
