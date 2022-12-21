5ffe99bfb35584255e7c2c1454920b462e128bca
테스트 <sk47kt@naver.com>
2022-12-21 19:01:07 +0900

    test4

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test Page' });
+  res.render('index', { title: '정성민페이지' });
 });
 
 module.exports = router;
